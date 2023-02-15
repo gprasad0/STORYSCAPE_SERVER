d26869e36be76ce5e8e54f72d4d1c2672bdaca44
gprasad0 <guru96392@gmail.com>
2023-02-15 16:27:10 +0530

    adding oauth

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
-import router from "./app/routes/routes.js";
+import router from "./app/routes/auth.js";
+import cookieSession from "cookie-session";
+import passport from "passport";
+import PassportSetup from "./passport.js";
 dotenv.config()
 const app = express();
 const mongoString = process.env.DATABASE_URL;
@@ -11,12 +14,23 @@ const mongoString = process.env.DATABASE_URL;
 // const routes = require('./app/routes/routes');
 // const cors = require('cors');
 app.use(express.json());
+app.use(
+    cookieSession({
+        name:"session",
+        keys:["cyberwolve"],
+        maxAge:24*60*60*100
+    })
+)
+app.use(passport.initialize());
+app.use(passport.session()); 
 app.use(cors({
-    origin: '*'
+    origin: '*',
+    credentials:true
 }));
 
-app.use('/api', router)
+// app.use('/api', router)
 
+app.use('/auth', router)
 
 mongoose.connect(mongoString);
 const database = mongoose.connection;
