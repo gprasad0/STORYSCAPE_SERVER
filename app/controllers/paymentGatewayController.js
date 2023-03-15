e2286bc4f9fd4a4176e793d2f503f302f78080a6
gprasad0 <guru96392@gmail.com>
2023-03-16 00:48:21 +0530

    changes for payment gateway

---
+++ app/controllers/paymentGatewayController.js
@@ -0,0 +1,26 @@
+const Razorpay = require('razorpay');
+
+
+const orderProcessController = async(req,res) => {
+
+    try{
+        let instance = new Razorpay({ key_id: process.env.RAZORPAY_KEY_ID, key_secret: process.env.RAZORPAY_KEY_SECRET })
+
+        let orderData = await instance.orders.create({
+          amount: 50000,
+          currency: "INR",
+          receipt: "receipt#1",
+          notes: {
+            key1: "value3",
+            key2: "value2"
+          }
+        })
+    
+        res.json({orderId:orderData.id,amount:orderData.amount,currency:orderData.currency})
+    } catch(e){
+        console.log("e====>",e)
+    }
+   
+}
+
+module.exports = {orderProcessController};
--- app/routes/chatGptRouter.js
+++ app/routes/chatGptRouter.js
@@ -3,13 +3,13 @@
 // import { composeDataController } from "../controllers/composeDataController.js";
 const express = require('express');
 const composeDataController = require("../controllers/composeDataController.js")
-
+const orderProcessController = require("../controllers/paymentGatewayController").orderProcessController
 const router = express.Router();
 const verifyJwt = require("../middleware/verifyJWT")
 // router.get('/getAll', sampleController);
-router.use(verifyJwt)
+// router.use(verifyJwt)
 router.post('/storyscape/compose', composeDataController)
-
+router.post('/makeOrder',orderProcessController)
 
 
 module.exports = router
